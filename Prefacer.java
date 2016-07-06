33ff4715fa290c4cb35a554e793a9c17665461e8
znaeb <urnotbeanz@gmail.com>
2016-07-06 16:01:53 -0400

    fixes

--- Prefacer.java
+++ Prefacer.java
@@ -7,17 +7,13 @@ public class Prefacer {
     public static String nirp = "";
     
     public static void preface(int q) {
-	int flux = 18;
-	for (int j = 0; j < 10000; j++) {
-	    flux = (int) Math.cbrt(flux);
-	}
-	
+
 	if (q > 0) { 
 	    nextChar += (int) ((Math.sqrt(nextChar) * 12)
 		+ (nextChar / 3) - (Math.sqrt(nextChar + 1))
-		+ (int) (Math.acos((double) nextChar) * Math.atan((double) nextChar))
+		+ (int) (Math.sin((double) nextChar))
 		- ((short) Math.cbrt((double) nextChar + 2)))
-	        - 70 - 39 - ((int) flux);
+	        - 109;
 	    nextChar++;
 	    // System.out.println("**" + (int) nextChar + "**");
 	    if (nextChar > 'Z' || nextChar < ' ') {
